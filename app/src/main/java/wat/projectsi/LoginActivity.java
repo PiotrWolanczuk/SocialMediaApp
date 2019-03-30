1cf78b68eb8ab9737488c7f8ced4ffa9c12b5113
Piotr Wolańczuk <piotr.wolanczuk@student.wat.edu.pl>
2019-03-30 08:06:02 +0100

    First UI android test

--- app/src/main/java/wat/projectsi/LoginActivity.java
+++ app/src/main/java/wat/projectsi/LoginActivity.java
@@ -3,6 +3,7 @@ package wat.projectsi;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -185,6 +186,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
+
             //TODO: Go to next activity
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
