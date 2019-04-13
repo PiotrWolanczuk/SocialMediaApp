2ed3dc8e3dee8bb8d25248c9fd7dacde26816bb6
Piotr Wolańczuk <piotr.wolanczuk@student.wat.edu.pl>
2019-04-13 14:24:17 +0200

    Choosing photo from  gallery

--- app/src/main/java/wat/projectsi/client/activity/NewPostActivity.java
+++ app/src/main/java/wat/projectsi/client/activity/NewPostActivity.java
@@ -1,10 +1,16 @@
 package wat.projectsi.client.activity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import wat.projectsi.R;
@@ -14,6 +20,9 @@ public class NewPostActivity extends AppCompatActivity {
     Button sendButton;
     Button addPhoto;
     EditText newPostText;
+
+    private static int RESULT_LOAD_IMAGE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +42,29 @@ public class NewPostActivity extends AppCompatActivity {
     }
 
     public void add_photo_from_gallery(View view) {
+        Intent gallery = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, RESULT_LOAD_IMAGE);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = (ImageView) findViewById(R.id.new_photo);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+            // String picturePath contains the path of selected Image
+        }
     }
 }
